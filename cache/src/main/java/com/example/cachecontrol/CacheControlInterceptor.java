22240d3c73c386b079f9bd3106f77d8b896dbbea
2yujeong <yjfflq4872@gmail.com>
2022-09-02 23:01:04 +0900

    feat: Cache Busting 적용

--- cache/src/main/java/com/example/cachecontrol/CacheControlInterceptor.java
+++ cache/src/main/java/com/example/cachecontrol/CacheControlInterceptor.java
@@ -4,19 +4,19 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.springframework.http.CacheControl;
 import org.springframework.http.HttpHeaders;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.servlet.HandlerInterceptor;
-import org.springframework.web.servlet.ModelAndView;
 
 public class CacheControlInterceptor implements HandlerInterceptor {
 
     @Override
-    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
-                           ModelAndView modelAndView) {
+    public boolean preHandle(final HttpServletRequest request, final HttpServletResponse response,
+                             final Object handler) {
         final String cacheControl = CacheControl
                 .noCache()
                 .cachePrivate()
                 .getHeaderValue();
         response.addHeader(HttpHeaders.CACHE_CONTROL, cacheControl);
+
+        return true;
     }
 }
