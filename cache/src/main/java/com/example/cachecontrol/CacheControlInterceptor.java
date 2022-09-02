2d2bae80e10b35b768d46ed35d4b77b6c65e209d
2yujeong <yjfflq4872@gmail.com>
2022-09-02 16:53:18 +0900

    feat: 모든 응답에서 휴리스틱 캐싱 제거

---
+++ cache/src/main/java/com/example/cachecontrol/CacheControlInterceptor.java
@@ -0,0 +1,22 @@
+package com.example.cachecontrol;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.http.CacheControl;
+import org.springframework.http.HttpHeaders;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+public class CacheControlInterceptor implements HandlerInterceptor {
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+                           ModelAndView modelAndView) {
+        final String cacheControl = CacheControl
+                .noCache()
+                .cachePrivate()
+                .getHeaderValue();
+        response.addHeader(HttpHeaders.CACHE_CONTROL, cacheControl);
+    }
+}
